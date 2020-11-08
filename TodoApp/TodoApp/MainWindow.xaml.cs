1fcfc700f87e43111afdd4b62bcedbd38a713e36
Vlad <valdisnn19@yandex.ru>
2020-11-08 11:27:02 +0300

    release -v1.0.1

--- TodoApp/TodoApp/MainWindow.xaml.cs
+++ TodoApp/TodoApp/MainWindow.xaml.cs
@@ -18,75 +18,60 @@ using System.Windows.Shapes;
 using TodoApp.Models; // подключение пространства имен Models
 using TodoApp.Services; // подключение пространства имен Services
 
-namespace TodoApp
-{
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
+namespace TodoApp {
+  /// <summary>
+  /// Логика взаимодействия для MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow: Window {
 
-        private readonly string PATH = $"{Environment.CurrentDirectory}\\todoDataList.json";
-        private BindingList<TodoModel> _todoDataList;  // КОНТЕЙНЕР
-        private FileIOService _fileIOService;
+    private readonly string PATH = $"{Environment.CurrentDirectory}\\todoDataList.json";
+    private BindingList < TodoModel > _todoDataList; // КОНТЕЙНЕР
+    private FileIOService _fileIOService;
 
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            _fileIOService = new FileIOService(PATH);
-            try
-            {
-                _todoDataList = _fileIOService.LoadData();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-                this.Close();
-            }
-
-            dgTodoList.ItemsSource = _todoDataList; // привязка списка к графической части 
-            _todoDataList.ListChanged += _todoDataList_ListChanged; // подписали метод
-        }
-
-        // сгенерированный метод
-        private void _todoDataList_ListChanged(object sender, ListChangedEventArgs e)
-        {
-           if(e.ListChangedType == ListChangedType.ItemAdded || 
-                e.ListChangedType == ListChangedType.ItemDeleted || 
-                e.ListChangedType == ListChangedType.ItemChanged)
-            {
-                try
-                {
-                    _fileIOService.SaveData(sender);
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.Message);
-                    this.Close();
-                }
-            }
-        }
-
-        private void AllItemsDelete(object sender, RoutedEventArgs e)
-        {
-            File.WriteAllText(PATH, "[]");
-            _fileIOService = new FileIOService(PATH);
-            try
-            {
-                _todoDataList = _fileIOService.LoadData();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-                this.Close();
-            }
-
-            dgTodoList.ItemsSource = _todoDataList; // привязка списка к графической части 
-            _todoDataList.ListChanged += _todoDataList_ListChanged; // подписали метод
-        }
+    public MainWindow() {
+      InitializeComponent();
     }
+
+    private void Window_Loaded(object sender, RoutedEventArgs e) {
+      _fileIOService = new FileIOService(PATH);
+      try {
+        _todoDataList = _fileIOService.LoadData();
+      }
+      catch(Exception ex) {
+        MessageBox.Show(ex.Message);
+        this.Close();
+      }
+
+      dgTodoList.ItemsSource = _todoDataList; // привязка списка к графической части 
+      _todoDataList.ListChanged += _todoDataList_ListChanged; // подписали метод
+    }
+
+    // сгенерированный метод
+    private void _todoDataList_ListChanged(object sender, ListChangedEventArgs e) {
+      if (e.ListChangedType == ListChangedType.ItemAdded || e.ListChangedType == ListChangedType.ItemDeleted || e.ListChangedType == ListChangedType.ItemChanged) {
+        try {
+          _fileIOService.SaveData(sender);
+        }
+        catch(Exception ex) {
+          MessageBox.Show(ex.Message);
+          this.Close();
+        }
+      }
+    }
+
+    private void AllItemsDelete(object sender, RoutedEventArgs e) {
+      File.WriteAllText(PATH, "[]");
+      _fileIOService = new FileIOService(PATH);
+      try {
+        _todoDataList = _fileIOService.LoadData();
+      }
+      catch(Exception ex) {
+        MessageBox.Show(ex.Message);
+        this.Close();
+      }
+
+      dgTodoList.ItemsSource = _todoDataList; // привязка списка к графической части 
+      _todoDataList.ListChanged += _todoDataList_ListChanged; // подписали метод
+    }
+  }
 }
--- TodoApp/TodoApp/Models/TodoModel.cs
+++ TodoApp/TodoApp/Models/TodoModel.cs
@@ -5,57 +5,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TodoApp.Models
-{
-    class TodoModel : INotifyPropertyChanged
-    {
-        // Дата создания
-        public DateTime CreationDate { get; set; } = DateTime.Now;
+namespace TodoApp.Models {
+  class TodoModel: INotifyPropertyChanged {
+    // Дата создания
+    public DateTime CreationDate {
+      get;
+      set;
+    } = DateTime.Now;
 
-        // Состояние задачи
-        private bool _isDone;
+    // Состояние задачи
+    private bool _isDone;
 
-        public bool IsDone
-        {
-            get { return _isDone; }
-            set 
-            { 
-                if(_isDone == value)
-                    return;
-                _isDone = value;
-                OnPropertyChanged("IsDone");
-            }
-        }
+    public bool IsDone {
+      get {
+        return _isDone;
+      }
+      set {
+        if (_isDone == value) return;
+        _isDone = value;
+        OnPropertyChanged("IsDone");
+      }
+    }
 
-        // Текст задачи
-        private string _text;
+    // Текст задачи
+    private string _text;
 
+    public string Text {
+      get {
+        return _text;
+      }
+      set {
+        if (_text == value) return;
+        _text = value;
+        OnPropertyChanged("Text");
+      }
+    }
 
-        public string Text
-        {
-            get { return _text; }
-            set 
-            {
-                if (_text == value )
-                    return;
-                _text = value;
-                OnPropertyChanged("Text");
-            }
-        }
+    public event PropertyChangedEventHandler PropertyChanged;
 
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected virtual void OnPropertyChanged(string propertyName = "")
-        {
-            // более простой вариант через обычное условие
-            /*if(PropertyChanged != null)
+    protected virtual void OnPropertyChanged(string propertyName = "") {
+      // более простой вариант через обычное условие
+      /*if(PropertyChanged != null)
             {
                 PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
             }
             */
 
-            // проверка с помощью тернарного оператора
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+      // проверка с помощью тернарного оператора
+      PropertyChanged ? .Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
+  }
 }
